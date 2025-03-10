3abe1ed46804eb775f31b994df01d844d9089ba1
Manuel Barone <baronemanuel05@gmail.com>
2025-03-10 18:12:23 +0100

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,11 @@ import { CustomNav } from "./components/CustomNav";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CustomHome from "./components/CustomHome";
 function App() {
-
   return (
     <>
       <BrowserRouter>
         <div className="gradient-generated vh-100 d-flex justify-content-center align-items-center">
-          <div className="rounded rounded-5 border border-2 father-color p-3">
+          <div className="rounded rounded-5 border border-2 father-color px-5 py-4">
             <CustomNav></CustomNav>
             <Routes>
               <Route path="/" element={<CustomHome></CustomHome>} />
