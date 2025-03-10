5e036364c0fe5ab23446c7fab9a1d300194c9fda
Manuel Barone <baronemanuel05@gmail.com>
2025-03-10 18:10:23 +0100

    single card template done

--- src/components/CustomHome.tsx
+++ src/components/CustomHome.tsx
@@ -4,7 +4,15 @@ function CustomHome() {
   return (
     <>
       <div>
-        <FlipperCard></FlipperCard>
+        <FlipperCard
+          image_url={`https://www.esa.int/var/esa/storage/images/esa_multimedia/videos/2025/03/hera_asteroid_mission_s_mars_flyby/26607880-1-eng-GB/Hera_asteroid_mission_s_Mars_flyby_card_full.png`}
+          authors={["ESA"]}
+          title="Hera asteroid mission’s Mars flyby"
+          url="https://www.esa.int/ESA_Multimedia/Videos/2025/03/Hera_asteroid_mission_s_Mars_flyby"
+          news_site="ESA"
+          summary="On  Wednesday 12 March 2025 ESA’s Hera spacecraft for planetary defence performs a flyby of Mars. The gravity of the red planet shifts the spacecraft’s trajectory towards its final destination of the Didymos binary asteroid system, shortening its trip by months and saving substantial fuel."
+          published_at="2025-03-10T15:00:00Z"
+        />
       </div>
     </>
   );
--- src/components/FlipperCard.tsx
+++ src/components/FlipperCard.tsx
@@ -1,7 +1,26 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from "react";
 import "./FlipperCard.css";
 
-function FlipperCard({ title, authors, url, image_url, news_site, summary }) {
+interface IFCard {
+  title: string;
+  authors: string[];
+  url: string;
+  image_url: string;
+  news_site: string;
+  summary: string;
+  published_at: string;
+}
+
+function FlipperCard({
+  title,
+  authors,
+  url,
+  image_url,
+  news_site,
+  summary,
+  published_at,
+}: IFCard) {
   useEffect(() => {
     const flipCard = document.getElementById("flip-card");
     if (flipCard) {
@@ -16,12 +35,42 @@ function FlipperCard({ title, authors, url, image_url, news_site, summary }) {
     }
   }, []);
 
+  const formattedDate: string = published_at
+    ? new Date(published_at).toLocaleDateString("it-IT", {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+      })
+    : "Data non disponibile";
+
   return (
     <>
       <div className="card" id="flip-card">
         <div className="card-inner" id="flip-card-inner">
-          <div className="card-front">Front Content</div>
-          <div className="card-back">Back Content</div>
+          <div
+            className="card-front"
+            style={{
+              backgroundImage: `url(${image_url})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+            }}
+          >
+            <div className="overlay"></div>
+            <h1 className="mb-2">
+              <a href={url} target="_blank" rel="noopener noreferrer">
+                {authors[0]}
+              </a>
+            </h1>
+            <p>{title}</p>
+            <p className="published-date">{formattedDate}</p>
+          </div>
+          <div className="card-back border border-1 border-light">
+            <a href={url} target="_blank" className="mb-3">
+              {news_site}
+            </a>
+            <p className="fs-6">{summary}</p>
+          </div>
         </div>
       </div>
     </>
