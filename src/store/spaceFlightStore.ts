7d9f5366a1e95e2488a8f92b3b188655e926e054
Manuel Barone <baronemanuel05@gmail.com>
2025-03-10 15:14:54 +0100

    update store

--- src/store/spaceFlightStore.ts
+++ src/store/spaceFlightStore.ts
@@ -2,10 +2,15 @@ import { create } from "zustand";
 
 interface SpaceState {
   listNow: object[];
+  navigationState: string;
   search: (newList: object[]) => void;
+  changeNavigationState: (newNavState: string) => void;
 }
 
 export const spaceFlightStore = create<SpaceState>((set) => ({
   listNow: [],
+  navigationState: "/",
   search: (newList) => set({ listNow: newList }),
+  changeNavigationState: (newNavState: string) =>
+    set({ navigationState: newNavState }),
 }));
