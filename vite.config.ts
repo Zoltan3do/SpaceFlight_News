f8f47de793944a63da705c30745414180564f601
Manuel Barone <baronemanuel05@gmail.com>
2025-03-17 01:05:06 +0100

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,44 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.(googleapis|gstatic)\.com\//,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "google-fonts-cache",
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 24 * 60 * 60,
+              },
+            },
+          },
+        ],
+      },
+      manifest: {
+        name: "SpaceFlightNews by Zoltan",
+        short_name: "SFNews",
+        description:
+          "The Spaceflight News  è un prodotto di Zoltan. È la più completa e aggiornata Web App per le notizie sui voli spaziali attualmente disponibile.",
+        theme_color: "#0168BA",
+        background_color: "#004580",
+        display: "standalone",
+        icons: [
+          {
+            src: "leoLogo.jpg",
+            sizes: "1120x1120",
+            type: "image/jpg",
+          },
+        ],
+      },
+    }),
+  ],
+});
